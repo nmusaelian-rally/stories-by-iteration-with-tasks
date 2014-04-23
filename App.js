b8c0d348fee907be7c905350304234a827aee964
N M <nmusaelian@rallydev.com>
2014-04-23 14:55:14 -0600

    added Tasks collection

--- App.js
+++ App.js
@@ -7,11 +7,11 @@ Ext.define('CustomApp', {
    		listeners:{
    			ready: function(combobox){
    				var iRef = combobox.getRecord().get('_ref'); 
-   				this._onStoriesLoaded(iRef);
+   				this._getData(iRef);
    			},
    			select: function(combobox){
    				var iRef = combobox.getRecord().get('_ref'); 
-   				this._onStoriesLoaded(iRef);
+   				this._getData(iRef);
    			},
    			scope: this 
    		}
@@ -19,6 +19,110 @@ Ext.define('CustomApp', {
    	this.add(iComboBox);
     },
     
+     _getData: function(iRef){
+        console.log('loading stories for ', iRef);
+   	Ext.create('Rally.data.WsapiDataStore',{
+   		model: 'User Story',
+   		autoLoad:true,
+   		fetch: ['Name','ScheduleState','FormattedID', 'Tasks'],
+   		filters:[
+   			{
+   				property : 'Iteration',
+   				operator : '=',
+   				value : iRef
+   			}
+   		],
+   		listeners: {
+   			load: this._onDataLoaded,
+   			scope:this
+   		}
+   	});
+     },
+     
+    _onDataLoaded: function(store, data){
+        console.log('onDataLoaded ', data);
+        var stories = [];
+        var pendingTasks = data.length;
+        
+        _.each(data, function(story) {
+            var owner = story.get('Owner');
+            var s  = {
+                FormattedID: story.get('FormattedID'),
+                Name: story.get('Name'),
+                _ref: story.get("_ref"),
+                Owner: (owner && owner._refObjectName) || 'None',
+                TaskCount: story.get('Tasks').Count,
+                Tasks: []
+            };
+                    
+            var tasks = story.getCollection('Tasks');
+            tasks.load({
+                fetch: ['FormattedID','Estimate'],
+                callback: function(records, operation, success){
+                    _.each(records, function(task){
+                        s.Tasks.push({_ref: task.get('_ref'),
+                                        FormattedID: task.get('FormattedID')
+                                    });
+                    }, this);
+                    
+                    --pendingTasks;
+                    if (pendingTasks === 0) {
+                        this._createGrid(stories);
+                    }
+                },
+                scope: this
+            });
+            stories.push(s);
+                    
+        }, this);
+                
+    },
+     _createGrid: function(stories) {
+        
+        if(this.down('#storyGrid')){
+            this.down('#storyGrid').destroy();
+        }
+        console.log('stories', stories);
+         this.add({
+            xtype: 'rallygrid',
+            itemId: 'storyGrid',
+            store: Ext.create('Rally.data.custom.Store', {
+                data: stories,
+                pageSize: 100
+            }),
+            
+            columnCfgs: [
+                {
+                   text: 'Formatted ID', dataIndex: 'FormattedID', xtype: 'templatecolumn',
+                    tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate')
+                },
+                {
+                    text: 'Name', dataIndex: 'Name'
+                },
+                {
+                    text: 'Owner', dataIndex: 'Owner'
+                },
+                {
+                    text: 'Task Count', dataIndex: 'TaskCount'
+                },
+                {
+                    text: 'Tasks', dataIndex: 'Tasks', 
+                    renderer: function(value) {
+                        var html = [];
+                        Ext.Array.each(value, function(task){
+                            html.push('<a href="' + Rally.nav.Manager.getDetailUrl(task) + '">' + task.FormattedID + '</a>')
+                        });
+                        return html.join(', ');
+                    }
+                }
+            ]
+            
+        });
+    }
+    
+    
+    
+ /*   
      _onStoriesLoaded: function(iRef){
    	console.log('loading stories for ', iRef);
    	
@@ -42,7 +146,7 @@ Ext.define('CustomApp', {
    		}
    	});
    },
-   
+  
    _updateGrid: function(_store){
    	if(this._grid === undefined){
    		this._createGrid(_store);
@@ -66,6 +170,6 @@ Ext.define('CustomApp', {
    	});
    	this.add(this._grid);
    }
-   
+ */  
    
 });
